6dcc4d148fab4d9007435e1b04c84ba5f9b90b2f
Micael Lopes <micaelopes32@gmail.com>
2018-09-03 14:58:19 -0300

    Solução final desafio Sequência de Caracteres

--- src/strings.rb
+++ src/strings.rb
@@ -2,6 +2,34 @@ class Strings
     # Recebe uma `string` e retorna a(s) sequência(s) de caracteres 
     # que mais se repete(m).
     def self.resolve(word)
-        
+        word.downcase!
+        # Guarda os caracteres e suas repetições
+        dict = {}
+        index = 0
+
+        while char = word[index]
+            initial_index = index
+            index += 1 while word[index] == char
+
+            # Quantidade de caracteres sequenciados
+            interval = index - initial_index
+            
+            # Caso não haja sequencia para char
+            if dict[char].nil? 
+                dict[char] = char * interval
+            # Caso já haja uma sequência anterior de char
+            elsif dict[char].length < interval 
+                dict[char] = char * interval
+            end
+        end
+
+        # Retorna somente as sequências maiores que 1
+        return dict.select{|k,v| v.length > 1}.values
     end
 end
+
+=begin
+File.readlines('./words.txt').each do |word|
+    puts Strings.resolve(word).inspect 
+end
+=end
--- test/test_strings.rb
+++ test/test_strings.rb
@@ -3,8 +3,13 @@ require_relative '../src/strings.rb'
 
 class TestStrings < Test::Unit::TestCase
     def test_resolve
-        assert_nil(Strings.resolve('Gol'))
-        assert_equal(["ooo"], Strings.resolve('Goool'))
-        assert_equal(["ooooo", "aaaaa"], Strings.resolve('Gooooolaaaaaçooo'))
+        assert_equal([], Strings.resolve('Gol'))
+        assert_equal(['ooo'], Strings.resolve('Goool'))
+        assert_equal(['ooooo', 'aaaaa'], Strings.resolve('Gooooolaaaaaçooo'))
+        
+        words = File.readlines('./words.txt')
+        assert_equal(['ooo', 'ggg'].sort, Strings.resolve(words[0]).sort)
+        assert_equal(['tt', 'cc'].sort, Strings.resolve(words[1]).sort)
+        assert_equal(['mm', 'cc', 'nn'].sort, Strings.resolve(words[2]).sort)
     end
 end
